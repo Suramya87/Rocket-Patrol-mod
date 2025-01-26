db40dd8518c1d641fa43f572db0d3b0ca214e276
Suramya87 <146380955+Suramya87@users.noreply.github.com>
2025-01-26 01:05:16 -0800

    first draft done

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 // Code Practice: Rocket patrol
 // Name: Suramya Shakya
-// Date: 01/13/2025
+// Date: 01/26/2025
 
+// Rocket Patrol variation 934683580 Plus (will think of something better later)
+// MOD LIST
+////////////////////////////////////////////////////////////////
 // 1-Point Tier
 // Create a new scrolling tile sprite for the background (1)
 // 3-Point Tier
@@ -12,6 +15,11 @@
 // Create a new enemy Spaceship type (w/ new artwork) that's smaller, moves faster, and is worth more points (5)
 // Implement a new timing/scoring mechanism that adds time to the clock for successful hits and subtracts time for misses (5)
 
+// doing this took me 10 hours i think. I am just leaving here for now because its late. 
+// I believe what I have is enough for 20 points but i might add more just to practice more.
+
+// I mostly used phaser documentation and alot of trial and error.
+// I needed to look up syntax and values for things like clock and randomization
 let config = {
     type: Phaser.AUTO,
     // width: 640,
